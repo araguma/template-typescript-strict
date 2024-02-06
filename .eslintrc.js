3b3eebbd32dfdbc5dca5a25185388ae769f6a153
araguma <itetbitb@gmail.com>
2024-02-06 06:23:53 -0500

    style: wrap extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 /* eslint-env node */
 
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/strict-type-checked', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'prettier',
+    ],
     plugins: ['@typescript-eslint'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
